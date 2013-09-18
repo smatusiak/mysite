8b9099bc793f636df741be3f390c03b7a5baef41
smatusiak <smatusiak@ubuntuVM.(none)>
2013-09-18 13:49:53 -0400

    another commit - urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from mysite.views import hello
+from mysite.views import hello, current_datetime
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
 )
