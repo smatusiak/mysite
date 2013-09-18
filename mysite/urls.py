6659a84933f1985a2ff532df2607ba848918705f
smatusiak <smatusiak@ubuntuVM.(none)>
2013-09-18 11:29:28 -0400

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from mysite.views import hello
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello),
+)
