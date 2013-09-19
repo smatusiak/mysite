c2bcc0e2c632b4c11b794efc846da2dd52637fdc
smatusiak <smatusiak@gmail.com>
2013-09-18 23:45:04 -0400

    end of chapter 4

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,4 @@
-from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse
+from django.shortcuts import render
 import datetime
 
 
@@ -9,9 +7,7 @@ def hello(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
-    return HttpResponse(html)
+    return render(request, 'current_datetime.html', {'current_date': now})
 
 def hours_ahead(request, offset):
     try:
@@ -19,5 +15,4 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body> in %s hour(s),it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    return render(request, 'hours_ahead.html', {'hour_offset': offset, 'next_time': dt})
